8ae8ec04ccc79b58057b512be6096bfb364b4f13
Claudia <Claudia@Claudia-PC>
2017-04-15 00:34:12 -0300

    ruta y pagina de ayuda.

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function ayudaAction()
     {
-        return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('OfertaBundle:Default:ayuda.html.twig');
     }
 }
