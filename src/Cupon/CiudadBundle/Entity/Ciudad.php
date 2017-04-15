7be601c425d2284f7e2637b894522fec106b294c
Claudia <Claudia@Claudia-PC>
2017-04-15 01:37:19 -0300

    Entidad Ciudad.

---
+++ src/Cupon/CiudadBundle/Entity/Ciudad.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Cupon\CiudadBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="ciudad")
+ */
+class Ciudad {
+    /** 
+     * @ORM\id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    
+    /** @ORM\Column(type="string", lenght=100) */
+    protected $nombre;
+    
+    /** @ORM\Column(type="string", lenght=100) */
+    protected $slug;
+    
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+    
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+    
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+    
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+    
+    public function __toString(){
+        return $this->getNombre();
+    }
+}
