0bdb1ab2ae6dc75a1ab1af705ddad5bc07a117ee
Francisco <Francisco@fmorales>
2017-04-17 00:58:40 -0300

    Entidades del proyecto.

--- src/Cupon/CiudadBundle/Entity/Ciudad.php
+++ src/Cupon/CiudadBundle/Entity/Ciudad.php
@@ -1,53 +1,94 @@
 <?php
 
 namespace Cupon\CiudadBundle\Entity;
+
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * Ciudad
+ *
+ * @ORM\Table()
  * @ORM\Entity
- * @ORM\Table(name="ciudad")
  */
-class Ciudad {
-    /** 
-     * @ORM\id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue
+class Ciudad
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    protected $id;
-    
-    /** @ORM\Column(type="string", lenght=100) */
-    protected $nombre;
-    
-    /** @ORM\Column(type="string", lenght=100) */
-    protected $slug;
-    
-    
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=100)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=100)
+     */
+    private $slug;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
     public function getId()
     {
         return $this->id;
     }
-    
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Ciudad
+     */
     public function setNombre($nombre)
     {
         $this->nombre = $nombre;
+
+        return $this;
     }
-    
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
     public function getNombre()
     {
         return $this->nombre;
     }
-    
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Ciudad
+     */
     public function setSlug($slug)
     {
         $this->slug = $slug;
+
+        return $this;
     }
-    
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
     public function getSlug()
     {
         return $this->slug;
-    }
-    
-    public function __toString(){
-        return $this->getNombre();
     }
 }
---
+++ src/Cupon/TiendaBundle/Entity/Tienda.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace Cupon\TiendaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tienda
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Tienda
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=100)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=100)
+     */
+    private $slug;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=10)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="salt", type="string", length=255)
+     */
+    private $salt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="text")
+     */
+    private $descripcion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="direccion", type="text")
+     */
+    private $direccion;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Cupon\CiudadBundle\Entity\Ciudad")
+     */
+    private $ciudad;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Tienda
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Tienda
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     * @return Tienda
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string 
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return Tienda
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     * @return Tienda
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string 
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     * @return Tienda
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string 
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set direccion
+     *
+     * @param string $direccion
+     * @return Tienda
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    /**
+     * Get direccion
+     *
+     * @return string 
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * Set ciudad
+     *
+     * @param string $ciudad
+     * @return Tienda
+     */
+    public function setCiudad(\Cupon\CiudadBundle\Entity\Ciudad $ciudad)
+    {
+        $this->ciudad = $ciudad;
+
+        return $this;
+    }
+
+    /**
+     * Get ciudad
+     *
+     * @return string 
+     */
+    public function getCiudad()
+    {
+        return $this->ciudad;
+    }
+    
+    public function __toString() {
+        return $this->getNombre();
+    }
+}
